39c50f57616073ae0027680c7f3de57a2b8c4692
seneshMH <senesh1senesh@gmail.com>
2022-08-29 16:20:31 +0530

    fix premake

--- premake5.lua
+++ premake5.lua
@@ -19,7 +19,7 @@ project "GLFW"
 	}
 
     filter "system:windows"
-		buildoption {"-std=c11","-lgdi32"}
+		buildoptions {"-std=c11","-lgdi32"}
 		systemversion "latest"
         staticruntime "On"
 
